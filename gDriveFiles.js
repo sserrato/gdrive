87792a8073884eb96ccaf3556fa1baf64a5f5cf9
sserrato <p.sergio.serrato@gmail.com>
2023-06-13 15:14:49 -0700

    updated to include columns of filetype.

--- gDriveFiles.js
+++ gDriveFiles.js
@@ -1,17 +1,17 @@
 function getMyFilesFromDrive() {
-    var myFiles = DriveApp.searchFiles('"me" in owners');
-    var sheet = SpreadsheetApp.getActive().getSheetByName("Files");
-    sheet.clear();
-    var rows = [];
-    rows.push(["ID", "Name", "Url", "DateCreated", "DateUpdated"]);
-    while(myFiles.hasNext()) {
-      var file = myFiles.next();
-      if(file != null) {
-        rows.push([file.getId(), file.getName(), file.getUrl(), file.getDateCreated(), file.getLastUpdated()]);
-      }
+  var myFiles = DriveApp.searchFiles('"me" in owners');
+  var sheet = SpreadsheetApp.getActive().getSheetByName("Files");
+  sheet.clear();
+  var rows = [];
+  rows.push(["ID", "Name", "Url", "Type", "DateCreated", "DateUpdated"]);
+  while(myFiles.hasNext()) {
+    var file = myFiles.next();
+    if(file != null) {
+      rows.push([file.getId(), file.getName(), file.getUrl(), file.getMimeType(), file.getDateCreated(), file.getLastUpdated()]);
     }
-    sheet.getRange(1,1,rows.length,5).setValues(rows);
-   }
+  }
+  sheet.getRange(1,1,rows.length,6).setValues(rows);
+ }
 
    function getMyFilesFromDrive() {
  var myFiles = DriveApp.searchFiles('"me" in owners');
