5642806aae7f1d00e69f340cfe2a32957b64478c
Serrato <p.sergio.serrato@gmail.com>
2024-08-05 11:29:38 -0500

    adding importCSV.js and modifying gDriveFiles.js

--- gDriveFiles.js
+++ gDriveFiles.js
@@ -11,20 +11,4 @@ function getMyFilesFromDrive() {
     }
   }
   sheet.getRange(1,1,rows.length,6).setValues(rows);
- }
-
-   function getMyFilesFromDrive() {
- var myFiles = DriveApp.searchFiles('"me" in owners');
- var sheet = SpreadsheetApp.getActive().getSheetByName("Files");
- sheet.clear();
- var rows = [];
- rows.push(["ID", "Name", "Url", "DateCreated", "DateUpdated"]);
- while(myFiles.hasNext()) {
-   var file = myFiles.next();
-   if(file != null) {
-     rows.push([file.getId(), file.getName(), file.getUrl(), file.getDateCreated(), file.getLastUpdated()]);
-   }
- }
- sheet.getRange(1,1,rows.length,5).setValues(rows);
-}
-// -- https://spreadsheet.dev/export-list-of-files-in-google-drive-to-google-sheets-using-apps-script
+ }// -- https://spreadsheet.dev/export-list-of-files-in-google-drive-to-google-sheets-using-apps-script
